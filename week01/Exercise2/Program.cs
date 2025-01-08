5b4b0001dcc942a1bd705c1f090d3f94679ee77f
trystandj <trystandj@gmail.com>
2025-01-07 20:41:12 -0500

    Week 1 exercises and week 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradeNumber = int.Parse(grade);
+        int lastNumber = gradeNumber % 10;
+
+        string letterGrade = "";
+        string sign = "";
+
+        if (gradeNumber >= 60 && gradeNumber <= 95)
+        {
+            if (lastNumber >= 5)
+            {
+                sign = "+";
+            }
+            else if (lastNumber < 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+        }
+        if (gradeNumber >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradeNumber >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradeNumber >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradeNumber >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letterGrade}{sign}.");
+
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine("Congrats! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Try studing more next time!");
+        }
     }
 }
