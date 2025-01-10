db8b423e764e0f08800462c1b6d28490357d4d71
trystandj <trystandj@gmail.com>
2025-01-10 18:42:32 -0500

    week 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Net.Security;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayMessage();
+        string userName = PromptName();
+        int userNumber = PromptNumber();
+
+        int squaredNumber = SquareNumber(userNumber);
+        Message(userName, squaredNumber);
+
+    }
+
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Hello! Welcome to the program.");
+    }
+
+    static string PromptName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptNumber()
+    {
+        Console.Write("Please enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int squaredNumber = number * number;
+        return squaredNumber;
+    }
+
+    static void Message (string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}.");
     }
 }
