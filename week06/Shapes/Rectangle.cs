d3819b16b1137df69b1be1a37d6ed0f20f97ed3f
trystandj <trystandj@gmail.com>
2025-02-10 20:09:51 -0500

    override

---
+++ week06/Shapes/Rectangle.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+class Rectangle : Shape
+{
+   private double _length;
+   private double _width;
+
+    public Rectangle(string color, double length, double width) : base (color){
+        _length = length;
+        _width = width;
+    }
+   public override double GetArea(){
+    return _width * _length;
+   }
+
+}
